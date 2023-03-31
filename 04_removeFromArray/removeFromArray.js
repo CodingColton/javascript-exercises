c3ab01787e02a3cf6ca6a75c0c2637e98c42a8fa
CodingColton <colton.bolton1998@gmail.com>
2023-03-30 22:02:34 -0500

    Pass first test in exercise 04_removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, number) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (number !== array[i]) {
+            newArray.push(array[i]);
+        };
+    };
+    return newArray;
 };
 
 // Do not edit below this line

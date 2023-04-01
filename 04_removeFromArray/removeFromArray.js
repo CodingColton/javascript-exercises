2d5e08093565061b78b755630104da71b6840178
CodingColton <colton.bolton1998@gmail.com>
2023-03-31 21:05:58 -0500

    Pass all tests for 04_removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,11 @@
-const removeFromArray = function(array, number) {
-    let newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (number !== array[i]) {
-            newArray.push(array[i]);
-        };
-    };
-    return newArray;
+const removeFromArray = function(array, ...numbers) {
+    for (let num = 0; num < numbers.length; num++) {
+        let index = array.indexOf(numbers[num]);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

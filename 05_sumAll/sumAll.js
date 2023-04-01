443ae56a62d17dd13c0a9e18abe3c2e7326f8669
CodingColton <colton.bolton1998@gmail.com>
2023-03-31 21:32:28 -0500

    Pass return ERROR tests for non-number and negative numbers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,11 +2,18 @@ const sumAll = function(...args) {
     let newArray = [];
     let total = 0;
     args.sort()
+    if (args[0] < 0) {
+        return 'ERROR';
+    }
     for (let i = args[(args.length - 1)]; i >= 0; i--) {
+        
         newArray.push(i);
     }
     newArray.sort();
     for (let i = 0; i < newArray.length; i++) {
+        if (typeof(newArray[i]) !== "number") {
+            return 'ERROR';
+        }
         total += parseInt(newArray[i]);
     }
     return total;

337e0d9b26af9b417e04718689c4d34272d015e6
CodingColton <colton.bolton1998@gmail.com>
2023-03-31 21:16:47 -0500

    Pass first test for numbers within range.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    let newArray = [];
+    let total = 0;
+    args.sort()
+    for (let i = args[(args.length - 1)]; i >= 0; i--) {
+        newArray.push(i);
+    }
+    newArray.sort();
+    for (let i = 0; i < newArray.length; i++) {
+        total += parseInt(newArray[i]);
+    }
+    return total;
 };
 
 // Do not edit below this line

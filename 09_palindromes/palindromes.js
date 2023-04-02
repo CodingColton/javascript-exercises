f019c38db930bc3a4553d890797e9bfe46d557f9
CodingColton <colton.bolton1998@gmail.com>
2023-04-02 16:36:54 -0500

    Complete 09_palindromes. Pass all tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let formattedPhrase = phrase.replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    let reversedPhrase = formattedPhrase.split("").reverse().join("");
+    return formattedPhrase === reversedPhrase ? true : false;
 };
 
 // Do not edit below this line

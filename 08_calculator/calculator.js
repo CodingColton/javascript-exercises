e1e558cd67b74405d7dbb5d55f53a9b71d9d2fdb
CodingColton <colton.bolton1998@gmail.com>
2023-04-02 16:06:50 -0500

    Pass tests up to sum function.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,17 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let totalSum = 0;
+	for (let index = 0; index < array.length; index++) {
+    totalSum += array[index];
+  }
+  return totalSum;
 };
 
 const multiply = function() {

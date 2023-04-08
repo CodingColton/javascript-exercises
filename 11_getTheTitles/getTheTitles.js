a04410cd2cc3dd03a6a2fd758d1ad478bd5cf7a4
CodingColton <colton.bolton1998@gmail.com>
2023-04-08 11:36:18 -0500

    Pass test for 11_getTheTitles and complete exercise.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object) {
+    booksList = [];
+    for (let book = 0; book < object.length; book++) {
+        booksList.push(object[book]["title"]);
+    }
+    return booksList;
 };
 
 // Do not edit below this line

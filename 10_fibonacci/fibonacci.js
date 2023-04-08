7b00548f665753456196173da14a611a74c69304
CodingColton <colton.bolton1998@gmail.com>
2023-04-08 11:27:56 -0500

    Pass 4th and 6th number tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (parseInt(number) < 0) return "OOPS";
+    fibonacciList = [1, 1];
+    i = 0
+    while (fibonacciList.length < parseInt(number)) {
+        fibonacciList.push(fibonacciList[i] + fibonacciList[fibonacciList.length - 1]);
+        i += 1;
+    }
+    
+    return fibonacciList[fibonacciList.length - 1];
 };
 
 // Do not edit below this line
